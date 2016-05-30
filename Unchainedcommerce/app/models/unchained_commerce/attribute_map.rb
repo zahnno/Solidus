3cf3bbbd6b52b436f18f443c0b5911f55d49502f
Patrick Zanowski <zahnno@gmail.com>
2016-05-30 17:29:50 -0400

    FIXED

--- Unchainedcommerce/app/models/unchained_commerce/attribute_map.rb
+++
@@ -1,7 +0,0 @@
-module UnchainedCommerce
-  class AttributeMap < ActiveRecord::Base
-	belongs_to :parent_attribute, :class_name => "UnchainedCommerce::Attribution", :inverse_of => :parent_relation
-	belongs_to :attribution, :class_name => "UnchainedCommerce::Attribution", :inverse_of => :child_relations
-	has_many :product_attributes
-  end
-end
